4b44cb95cab37b28564654e4437aba0eebcf2183
Łukasz Wartałowicz <wartalowiczl@gmail.com>
2022-03-09 17:03:41 +0100

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
+let computerMove = 'nieznany ruch';
+
 if (randomNumber == 1){ computerMove = 'kamień';
 } else if (randomNumber == 2){ computerMove = 'papier';
 } else { computerMove = 'nożyce';
 }
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+} else {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
